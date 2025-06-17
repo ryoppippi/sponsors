bfdc8905d473cb953f4ced5c1f0ff9caa0a81bc2
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-06-17 17:41:58 +0100

    feat: add sponsorkit

---
+++ sponsorkit.config.js
@@ -0,0 +1,9 @@
+import { defineConfig, tierPresets } from 'sponsorkit'
+
+export default defineConfig({
+  github: {
+    login: 'ryoppippi',
+    type: 'user',
+  },
+  outputDir: import.meta.dirname,
+})
